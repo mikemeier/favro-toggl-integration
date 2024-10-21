0ca9b1dd8325cc28e29bd7e87d6a727c2ef4d4bc
Mike Meier <mike@gotom.io>
2024-10-21 12:06:48 +0200

    bump version

--- build/favro-toggl.user.js
+++ build/favro-toggl.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Favro - Toggl Timer
 // @namespace    https://www.gotom.io/
-// @version      1.21.0
+// @version      1.22.0
 // @license      MIT
 // @author       Mike Meier
 // @match        https://favro.com/*
